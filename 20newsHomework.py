58d82befb3cf0a380e5060e7a3003ea6a93e69a1
QITAO SHEN <652870722@qq.com>
2018-09-30 09:06:11 +0800

    实验二
    实验二

--- 20newsHomework.py
+++ 20newsHomework.py
@@ -16,6 +16,7 @@ import numpy as np
 import json
 from nltk.stem import PorterStemmer
 ps = PorterStemmer()
+import math
 
 Newspath='D:\pythonCode\\20news-18828'
 folders=[f for f in os.listdir(Newspath)]
@@ -104,9 +105,12 @@ def flatten(list):
     return new_list
 
 list_of_words = []
-for document in pathname_list:
+doc_num=1
+for document in doc_train:
 #        print(document)
         list_of_words.append(flatten(tokenize(document)))
+        print(doc_num,'/',len(pathname_list))
+        doc_num=doc_num+1
 
 np_list_of_words=np.asarray(flatten(list_of_words))
 words,counts=np.unique(np_list_of_words,return_counts=True)
@@ -126,7 +130,88 @@ for doc_words in list_of_words:
     for i in range(len(w)):
         dictionary[doc_num][w[i]]=c[i]
     doc_num=doc_num+1
+    print(doc_num,'/',len(list_of_words))
 
+
+classDictionary={}
+for i in range(1,21):
+    classDictionary[i]={}
+class_num=1
+for i in range(1,len(doc_train)+1):
+    print(i,'/',len(doc_train))
+    for foldernames in folders:
+        if Y_train[i-1]==foldernames:
+            for f in features:
+                if f in dictionary[i].keys():
+                    if f not in classDictionary[folders.index(foldernames)+1].keys():
+                        classDictionary[folders.index(foldernames)+1][f]=dictionary[i][f]
+                    else:
+                        classDictionary[folders.index(foldernames)+1][f]=classDictionary[folders.index(foldernames)+1][f]+dictionary[i][f]
+                else:
+                     pass
+        else :
+            pass
+        
+classLength=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for i in range (0,20):
+    for word in classDictionary[i+1].keys():
+        classLength[i]=classLength[i]+classDictionary[1+i][word]
+        
+test_result=[]
+count_file=0
+for foldername in folders:
+    clf_lst=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for document in doc_test:
+        if Y_test[doc_test.index(document)]==foldername:
+            score_lst=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            print(count_file,'/',4707)
+            count_file=count_file+1
+            document_word=flatten(tokenize(document))
+            for i in range(1,21):
+                for word in document_word:
+                    if(word in features):
+                        if word in classDictionary[i].keys():
+                            percent=classDictionary[i][word]/(classLength[i-1])
+                            score_lst[i-1]= score_lst[i-1]+math.log(percent)
+                        else:
+                            percent=1/(classLength[i-1]+len(features))
+                            score_lst[i-1]= score_lst[i-1]+math.log(percent)
+                    else:
+                         pass
+            a=score_lst.index(max(score_lst))
+            clf_lst[a]=clf_lst[a]+1
+    test_result.append(clf_lst)
+    
+    
+print (test_result)
+recall=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+precise=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+predict_num=0
+recall_num=0
+#算精度precise,recall
+for i in range(0,20):
+    for j in range(0,20):
+        predict_num=predict_num+test_result[j][i]
+        recall_num=recall_num+test_result[i][j]
+    precise[i]=test_result[i][i]/predict_num*100
+    predict_num=0
+    recall[i]=test_result[i][i]/recall_num*100
+    recall_num=0
+    
+#输出表头
+print('\t\t\t   precise\t\trecall\t\tscore')
+    
+for i in range(0,20):
+#    accurate=test_result[i][i]/(sum(test_result[i]))*100
+    print(folders[i].rjust(25),'   ',end='')
+    #print('\t')
+ #   print('%.1f'%accurate,'\t\t',end='')
+    print('%.1f'%precise[i],'\t\t',end='')
+    print('%.1f'%recall[i],'\t\t',end='')
+    score=2/((1/recall[i])+(1/precise[i]))
+    print('%.1f'%score)
+#    print('\n')
+'''
 #homework1
 Train=[]
 for k in dictionary.keys():
@@ -137,6 +222,8 @@ for k in dictionary.keys():
         else:
             row.append(0)
     Train.append(row)
+'''
+    
 '''
 for doc_words in list_of_words:
     #print(doc_words)
@@ -164,9 +251,18 @@ for doc_words in list_of_words:
     print(doc_num,'/',len(list_of_words))
     doc_num = doc_num + 1
 '''
-    
-
-
+'''   
+X_Train=[]
+for k in dictionary.keys():
+    row=[]
+    for f in features:
+        if(f in dictionary[k].keys()):
+            row.append(dictionary[k][f])
+        else:
+            row.append(0)
+    X_Train.append(row)
+    print(k,'/',len(dictionary.keys()))
+'''
 
 
 
